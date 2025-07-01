d5656960083909439e79f25bdcd7c2faa3d948be
Fan Li <lexafaxine@gmail.com>
2025-07-01 20:54:23 +0900

    initial setup with hardcoded PoC

--- main.ts
+++ main.ts
@@ -1,81 +1,41 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface GoogleCalendarImporterSettings {
+	calendarMarker: string;
+	enabledForDailyNotes: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: GoogleCalendarImporterSettings = {
+	calendarMarker: '<!-- google-calendar -->',
+	enabledForDailyNotes: true
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class GoogleCalendarImporter extends Plugin {
+	settings: GoogleCalendarImporterSettings;
 
 	async onload() {
 		await this.loadSettings();
+		this.registerEvent(
+			this.app.workspace.on('file-open', (file) => {
+				if (file && this.isDailyNote(file)) {
+					this.injectCalendarEvents(file);
+				}
+			})
+		);
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'import-google-calendar',
+			name: 'Import Google Calendar events and tasks',
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				const activeFile = this.app.workspace.getActiveFile();
+				if (activeFile) {
+					this.injectCalendarEvents(activeFile);
+				} else {
+					new Notice('No active file to import events to');
 				}
 			}
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new GoogleCalendarSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -89,28 +49,33 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	isDailyNote(file: TFile): boolean {
+		const dailyNotesFormat = /\d{4}-\d{2}-\d{2}/;
+		return dailyNotesFormat.test(file.basename);
+	}
+
+	async injectCalendarEvents(file: TFile) {
+		if (!this.settings.enabledForDailyNotes && this.isDailyNote(file)) {
+			return;
+		}
+		const content = await this.app.vault.read(file);
+		if (content.includes(this.settings.calendarMarker)) {
+			return;
+		}
+		const calendarBlock = `\n\n${this.settings.calendarMarker}\n## 📅 Today's Calendar Events\n\n- 🕘 9:00 AM - Team Standup Meeting\n- 🕐 1:00 PM - Project Review\n- 🕒 3:30 PM - Client Call\n\n**Tasks:**\n- [ ] Review quarterly reports\n- [ ] Update project timeline\n- [ ] Prepare presentation slides\n\n${this.settings.calendarMarker}\n`;
+
+		const newContent = content + calendarBlock;
+		await this.app.vault.modify(file, newContent);
+		
+		new Notice('Calendar events imported!');
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class GoogleCalendarSettingTab extends PluginSettingTab {
+	plugin: GoogleCalendarImporter;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: GoogleCalendarImporter) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,13 +86,23 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('Enable for Daily Notes')
+			.setDesc('Automatically inject calendar events when opening daily notes')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.enabledForDailyNotes)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.enabledForDailyNotes = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Calendar Marker')
+			.setDesc('HTML comment used to mark injected calendar content')
+			.addText(text => text
+				.setPlaceholder('<!-- google-calendar -->')
+				.setValue(this.plugin.settings.calendarMarker)
+				.onChange(async (value) => {
+					this.plugin.settings.calendarMarker = value;
 					await this.plugin.saveSettings();
 				}));
 	}
